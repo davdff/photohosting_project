8a0ea7fb1c4598081f0ba1be1a1147de2b4d1ac5
Nikita <nikitadave842@gmail.com>
2023-07-12 23:43:10 +0200

    module1-task8

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import { checkActiveButton } from "./miniPictures.js";
 const userPhotos = await fetch("http://localhost:4001/photos")
     .then((response) => response.json())
     .then((json) => {
@@ -12,9 +13,17 @@ const userComments = await fetch("http://localhost:4001/comments")
     })
     .catch(() => alert("Ошибка при получении данных!"));
 
+
+
 for (let i = 0; i < userPhotos.length; i++) {
     userPhotos[i].comments = userComments;
 }
 
+if (userPhotos && userComments) {
+    checkActiveButton()
+    const filtersBlock = document.querySelector('.img-filters')
+    filtersBlock.classList.remove('img-filters--inactive')
+
+}
 export { userPhotos }
 export { userComments } 
